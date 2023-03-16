a74f78279dbf1c86079866d6c80e45c7a0f488ce
wuhaoming <wuhaoming@ghzhushou.com>
2023-03-16 11:41:54 +0800

    feat: 请求次数限制

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './filters/all-exception.filter';
-
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 async function bootstrap() {
   // createLogger of Winston
 
   const app = await NestFactory.create(AppModule, {
+    cors: true, // 允许跨域
     // 关闭整个nestjs日志
     // logger: false
   });
@@ -27,6 +29,16 @@ async function bootstrap() {
 
   // 弊端：无法使用DI -> 无法访问userService，需要在app.module的provider引入guard
   // app.useGlobalGuards()
+
+  // helmet头部安全
+  app.use(helmet());
+  // rateLimit限流
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15mins
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   const port = 3000;
   await app.listen(port);
